6ed7919b91b32222ccd53a7cebaee8aa2275ae9e
Antônio César <antonio.contato.eng@gmail.com>
2023-03-12 20:32:53 -0300

    criando a classe cliente e estrutura de fluxo

--- cliente.py
+++ cliente.py
@@ -1,12 +1,57 @@
 import socket
+import os
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Cliente:
 
-cliente.connect(('localhost', 6969))
+    def __init__(self, HOST, PORT):
+        self.cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.HOST = HOST
+        self.PORT = PORT
+        
+    
+    def iniciar_conexao(self):
+        self.cliente.connect((self.HOST, self.PORT))
+    
+    def testar_conexao(self):
+        mensagem = "racobaldo".encode()
+        self.cliente.send(mensagem)
+        data = self.cliente.recv(1024).decode()
+        print(data)
 
-mensagem = "racobaldo".encode()
-cliente.send(mensagem)
+    def encerrar_conexao(self):
+        print("> Encerrando conexão")
+        self.cliente.close()
 
-data = cliente.recv(1024).decode()
-print(data)
-cliente.close()
+    # TODO REFORMULAR ISSO
+    def criar_usuario(self):
+        self.cliente.send("01".encode())
+    
+    def start(self):
+        # PRINTAR TODAS AS OPCOES DO CLIENTE E MANTER DENTRO DE UM LOOP TIPO UM MENU
+        self.iniciar_conexao()
+        os.system('pause')
+
+        while True:
+            print("> Escolha as opções:")
+            print("[1] racobaldo")
+            print("[2] Criar usuario")
+            print("[0] Finalizar Conexão")
+            resposta = int(input())
+
+            if resposta == 1:
+                self.testar_conexao()
+                os.system("pause")
+            elif resposta == 2:
+                self.criar_usuario()
+                os.system("pause")
+            elif resposta == 0:
+                self.encerrar_conexao()
+                break
+            else:
+                print("> Resposta Inválida!")
+                os.system("pause")
+
+
+if __name__ == '__main__':
+    cliente = Cliente('localhost', 6969)
+    cliente.start()
--- servidor.py
+++ servidor.py
@@ -7,29 +7,66 @@ class Servidor:
         self.servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.HOST = HOST
         self.PORT = PORT
-        self.servidor.bind((self.HOST, self.PORT))
+        self.servidor.bind((self.HOST, self.PORT))      # Informando o local e endereço do servidor
+        self.iniciar_banco()
+        self.listen(10)                               # Numero total de acessos simultâneos (o servidor recusará caso exceda)
     
     def listen(self, num_conexoes):
-        print('aguardando conexoes')
+        print('> Aguardando conexões')
         self.servidor.listen(num_conexoes)
     
-    def gerenciar_cliente(self, mensagem):
+    def gerenciar_cliente(self, conexao, endereco):
+        print(f"> Conexão encontrada! -> {endereco}")
+        conexao.send("> Conexão feita com sucesso!".encode())
+
+        while True:
+            try:
+                data = conexao.recv(1024).decode()
+                if not data:
+                    break
+
+                if data == 'racobaldo':
+                    data += ' + Gabi + Carro'
+                    conexao.send(data.encode())
+                
+                # MINHA SUGESTAO PARA TRATAR DAS REQUISIÇÕES DO USUÁRIO
+                if data[0]+data[1] == "01":
+                    self.criar_usuario()
+
+            except ConnectionResetError:
+                break
+
+        conexao.close()
+        print(f"> Conexão encerrada: {endereco}")
+
+
+    # Metodo *G E N E R I C O* para iniciar um banco *G E N E R I C O*
+    def iniciar_banco(self):
+
+        self.banco = sqlite3.connect('usuarios.db')
+
+        # Cria uma tabela para armazenar os usuários (se ela não existir)
+        self.banco.execute('''CREATE TABLE IF NOT EXISTS usuarios
+             (ID INTEGER PRIMARY KEY AUTOINCREMENT,
+             NOME TEXT NOT NULL,
+             EMAIL TEXT NOT NULL,
+             SENHA TEXT NOT NULL);''')
+
+    def salvar_banco(self):
+        self.banco.commit()
+
+    def encerrar_conexao_banco(self):
+        self.banco.close()
+
+    def criar_usuario(self):
         pass
 
     def start(self):
-        #TODO COLOCAR THREADs
-        conexao, endereco = self.servidor.accept()
-
         while True:
-            data = conexao.recv(1024).decode()
-            if not data:
-                break
+            conexao, endereco = self.servidor.accept()
+            thread = threading.Thread(target=self.gerenciar_cliente, args=(conexao, endereco))
+            thread.start()
 
-            #TODO TRATAR TODAS AS REQUISIÇÕES
-            data += ' + Gabi + Carro'
-            conexao.send(data.encode())
-
-
-server = Servidor('localhost', 6969)
-server.listen(1)
-server.start()
+if __name__ == '__main__':
+    server = Servidor('localhost', 6969)
+    server.start()
